958c8e52954ecb48da7a2e6af76ded7091c38aa6
Soro-David <soroddavid63@gmail.com>
2025-11-05 11:14:34 +0000

    ajout de picture et register de user

--- resources/views/paroisse/event/index.blade.php
+++ resources/views/paroisse/event/index.blade.php
@@ -74,7 +74,7 @@
 
 @push('js')
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="{{ asset('DataTables/dataTables.min.js') }}"></script>
+<script src="{{ ('DataTables/dataTables.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
