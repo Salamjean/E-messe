048c571380824903666fbb4daa829480bfee0057
Soro-David <soroddavid63@gmail.com>
2025-11-05 11:18:10 +0000

    ajout de picture et register de user

--- resources/views/paroisse/event/index.blade.php
+++ resources/views/paroisse/event/index.blade.php
@@ -3,6 +3,7 @@
 @push('css')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{ asset('DataTables/dataTables.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
@@ -74,7 +75,9 @@
 
 @push('js')
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="{{ ('DataTables/dataTables.min.js') }}"></script>
+<script src="{{ asset('DataTables/dataTables.min.js') }}"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
