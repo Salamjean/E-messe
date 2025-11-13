c3f7ecc0cf0f3a9a3b2a5f6f828aa7f2d02a8906
Soro-David <soroddavid63@gmail.com>
2025-11-13 14:04:45 +0000

    13/11/25: notif detail messe

--- app/Http/Controllers/Paroisse/Demande/DemandeController.php
+++ app/Http/Controllers/Paroisse/Demande/DemandeController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use App\Notifications\MesseConfirmeeNotification;
+use App\Notifications\MesseAnnuleeNotification;
+use App\Notifications\MesseCelebreeNotification;
 use PDF;
 
 class DemandeController extends Controller
